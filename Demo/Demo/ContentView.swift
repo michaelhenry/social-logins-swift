33c241c7bf8d0111d6f4f27d0a3951e9faa5ef40
Michael Henry <me@iamkel.net>
2025-08-05 23:44:34 +1000

    Update demo project

--- Demo/Demo/ContentView.swift
+++ Demo/Demo/ContentView.swift
@@ -1,17 +1,50 @@
+import ComposableArchitecture
+import Foundation
+import Perception
+import SocialLoginsFeature
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+    @Perception.Bindable
+    private var store: StoreOf<SocialLoginsFeature>
 
-#Preview {
-    ContentView()
+    init(store: StoreOf<SocialLoginsFeature>) {
+        self.store = store
+    }
+
+    var body: some View {
+        WithPerceptionTracking {
+            List {
+                if let _ = store.googleIdToken {
+                    Button("Google Logout") {
+                        store.send(.logoutGoogle)
+                    }
+                } else {
+                    Button("Google Login") {
+                        store.send(.loginWithGoogle)
+                    }
+                }
+
+                if let _ = store.facebookIdToken {
+                    Button("Facebook Logout") {
+                        store.send(.logoutFacebook)
+                    }
+                } else {
+                    Button("Facebook Login") {
+                        store.send(.loginWithFacebook(.init(nonce: "123")))
+                    }
+                }
+
+                if let _ = store.appleIdToken {
+                    Button("Apple Logout") {
+                        store.send(.logoutApple)
+                    }
+                } else {
+                    Button("Apple Login") {
+                        store.send(.loginWithApple(.init(nonce: "123")))
+                    }
+                }
+            }
+        }
+    }
 }
--- Demo/Demo/DemoApp.swift
+++ Demo/Demo/DemoApp.swift
@@ -1,10 +1,37 @@
+import FacebookCore
+import GoogleSignIn
+import SocialLoginsFeature
 import SwiftUI
 
 @main
 struct DemoApp: App {
+    init() {
+        /// Facebook Login - Configs
+        FacebookCore.Settings.shared.appID = "$(FB_APP_ID)"
+        FacebookCore.Settings.shared.clientToken = "$(FB_CLIENT_TOKEN)"
+        FacebookCore.Settings.shared.displayName = "$(FB_APP_DISPLAY_NAME)"
+
+        /// Google Login - Configs
+        let config = GIDConfiguration(clientID: "GOOGLE_CLIENT_ID", serverClientID: "GOOGLE_SERVER_CLIENT_ID")
+        GIDSignIn.sharedInstance.configuration = config
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(store: .init(initialState: .init()) { SocialLoginsFeature()._printChanges() })
+                .onOpenURL { url in
+                    ApplicationDelegate.shared.application(
+                        UIApplication.shared,
+                        open: url,
+                        sourceApplication: nil,
+                        annotation: [UIApplication.OpenURLOptionsKey.annotation]
+                    )
+                    GIDSignIn.sharedInstance.handle(url)
+                }
+                .onAppear {
+                    ApplicationDelegate.shared.application(UIApplication.shared, didFinishLaunchingWithOptions: nil)
+                    GIDSignIn.sharedInstance.restorePreviousSignIn { _, _ in }
+                }
         }
     }
 }
