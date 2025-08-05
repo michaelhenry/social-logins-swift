07138469a6312d0d81829e65ae254a28b3269da7
Michael Henry <me@iamkel.net>
2025-08-05 22:23:33 +1000

    initial commit

---
+++ Demo/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Michael Henry Pantaleon on 5/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
